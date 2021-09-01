2e7f3f4b61aba7104ae6cd9a33a76ec7930b5d2b
Ravi Ranjan Kumar <kmrraviranjan1@gmail.com>
2021-09-01 13:55:16 +0530

    basic file structure completed

---
+++ src/components/Dsa.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Dsa = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Dsa
---
+++ src/components/Hr.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Hr = () => {
+    const questions=['Introduece yourself','What are your strengths','What are your weakness','Why do you want to work for company','How did you hear about the position','What is your career objective','Where do you see yourself after 5 years','What is your greatest personal/professional achievement','Tell me about a team project you build? your role and achievement','How does your perfect day/work day look like? Take me through it','Why you join masai school','Your experience at masai school','Why should we hire you? How will you be an asset for our company?']
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Hr
